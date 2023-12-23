4188854ceab2af1da991e6c57f232d00802fd478
Hulow <viq.hlw@gmail.com>
2023-12-23 23:14:42 +0100

    feat: add 4 sub sections to main #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,10 @@ export default function Home() {
       <div className="container">
         <header>Header</header>
         <div className="main">
-          <div className="main-section">
-            <div className="card-main-section">card-main-section-11</div>
-            <div className="card-main-section">card-main-section-11</div>
-          </div>
-          <div className="main-section">
-            <div className="card-main-section">card-main-section-11</div>
-            <div className="card-main-section">card-main-section-11</div>
-          </div>
+          <div className="main-section"></div>
+          <div className="main-section"></div>
+          <div className="main-section"></div>
+          <div className="main-section"></div>
         </div>
         <footer>Footer</footer>
       </div>
