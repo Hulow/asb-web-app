ef335c398c5e7ec8a2b408716beff10caa5feece
Hulow <viq.hlw@gmail.com>
2023-12-24 19:42:00 +0100

    chore: update app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,29 +5,18 @@ import { StationButton } from "@/components/buttons/station/StationButton";
 
 export default function Home() {
   return (
-    <body>
-      <div className="container">
-        <header></header>
-        <div className="main">
-          <div className="section">
-            <div className="sub-section">
-              <Logo></Logo>
-            </div>
-            <div className="sub-section"></div>
-          </div>
-          <div className="section"></div>
-          <div className="section"></div>
-          <div className="section info">
-            <div className="sub-section">
-              <MeasurementButton></MeasurementButton>
-            </div>
-            <div className="sub-section">
-              <StationButton></StationButton>
-            </div>
-          </div>
-        </div>
-        <footer></footer>
+    <div className="container">
+      <div className="sub-container">
+        <Logo />
       </div>
-    </body>
+      <div className="sub-container">
+        <div className="container-component">
+          <StationButton />
+        </div>
+        <div className="container-component">
+          <MeasurementButton />
+        </div>
+      </div>
+    </div>
   );
 }
