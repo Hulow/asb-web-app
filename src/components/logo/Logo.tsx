0c10ab964258c82ad1147db0b28ae2d8edd7af20
Hulow <viq.hlw@gmail.com>
2023-12-24 19:40:23 +0100

    chore: update logo component

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -3,8 +3,16 @@ import Image from "next/image";
 
 export function Logo() {
   return (
-    <div className="logo">
-      <Image priority src="/ASB.svg" width={500} height={500} alt="ASB Logo" />
+    <div className="logo-container">
+      <div className="image-logo-container">
+        <Image
+          priority
+          src="/ASB.svg"
+          width={500}
+          height={500}
+          alt="ASB Logo"
+        />
+      </div>
     </div>
   );
 }
