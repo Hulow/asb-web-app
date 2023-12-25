88e11006df29645faefded5f66d5ab002a31a4b4
Hulow <viq.hlw@gmail.com>
2023-12-25 01:23:29 +0100

    chore: update app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import { Logo } from "@/components/logo/Logo";
 import { MeasurementButton } from "@/components/buttons/measurement/MeasurementButton";
 import "./globals.css";
 import { StationButton } from "@/components/buttons/station/StationButton";
+import { config } from "@/config/config";
 
 export default function Home() {
+  const logoConfig = config.logo;
   return (
     <div className="container">
       <div className="sub-container">
