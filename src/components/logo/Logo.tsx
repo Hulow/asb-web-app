1a81bc538154642bd6dc4d441999d666f77d5010
Hulow <viq.hlw@gmail.com>
2023-12-25 21:38:06 +0100

    chore: update logo component

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -4,15 +4,7 @@ import Image from "next/image";
 export function Logo() {
   return (
     <div className="logo-container">
-      <div className="image-logo-container">
-        <Image
-          priority
-          src="/ASB.svg"
-          width={500}
-          height={500}
-          alt="ASB Logo"
-        />
-      </div>
+      <Image priority src="/ASB.svg" width={500} height={500} alt="ASB Logo" />
     </div>
   );
 }
@@ -20,15 +12,13 @@ export function Logo() {
 export function SecondaryLogo() {
   return (
     <div className="secondary-logo-container">
-      <div className="image-logo-container">
-        <Image
-          priority
-          src="/ASB_secondary.svg"
-          width={60}
-          height={60}
-          alt="ASB Logo"
-        />
-      </div>
+      <Image
+        priority
+        src="/ASB_secondary.svg"
+        width={60}
+        height={60}
+        alt="ASB Logo"
+      />
     </div>
   );
 }
