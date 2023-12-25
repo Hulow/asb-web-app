6654e3187c1b42a8ddfab5db227600d73d972a3d
Hulow <viq.hlw@gmail.com>
2023-12-25 04:27:56 +0100

    feat: setup owners page

---
+++ src/app/owners/page.tsx
@@ -0,0 +1,48 @@
+import { SecondaryLogoV2 } from "@/components/logo/Logo";
+import "./page.css";
+import { config } from "@/config/config";
+import { httpClient, FetchOptions } from "@/lib/http-client";
+import { OwnersOverview } from "@/types/owners";
+
+export default async function Owners() {
+  const endpoint = config.endpoints.owner.ownerOverview;
+  const fetchOptions: FetchOptions = {
+    method: "get",
+    headers: {
+      Authorization: config.asbKeyUrl,
+    },
+  };
+  const ownersOverview: OwnersOverview = await httpClient(
+    endpoint,
+    fetchOptions
+  );
+  const owners = ownersOverview.owners;
+  return (
+    <div className="owners-container">
+      <div className="owners-header">
+        <h1>OWNERS </h1>
+      </div>
+      <div className="cards-owner">
+        {owners.map((owner) => {
+          return (
+            <div className="card-owner" key={owner.owner.ownerUid}>
+              <div className="card-owner-header">
+                <h2>{owner.owner.ownername}</h2>
+              </div>
+              <div className="card-owner-title">
+                <SecondaryLogoV2 />
+              </div>
+              <div className="card-owner-body">
+                {owner.cabinets.map((cabinet) => {
+                  return (
+                    <h4 key={cabinet.cabinetUid}>{cabinet.productName}</h4>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
