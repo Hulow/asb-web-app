1aa3e6e10a22fa8a1a299cd05a4596e3ef70da5a
Hulow <viq.hlw@gmail.com>
2023-12-26 04:49:27 +0100

    feat: add owners page

--- src/app/owners/page.tsx
+++ src/app/owners/page.tsx
@@ -1,35 +1,35 @@
 import "./page.css";
 import { config } from "@/config/config";
 import { httpClient, FetchOptions } from "@/lib/http-client";
-import { OwnersOverview } from "@/types/owners";
+import { OwnersOverview } from "@/types/owner";
 import { CardOwner } from "@/components/cards/owner/OwnerCard";
+import { sanitizeOwnername } from "@/utils/utils";
+import Link from "next/link";
 
 export default async function Owners() {
   const endpoint = config.endpoints.owner.ownerOverview;
-  const fetchOptions: FetchOptions = {
-    method: "get",
-    headers: {
-      Authorization: config.asbKeyUrl,
-    },
-  };
-  const ownersOverview: OwnersOverview = await httpClient(
-    endpoint,
-    fetchOptions
-  );
+  const ownersOverview: OwnersOverview = await httpClient("get", endpoint);
   const owners = ownersOverview.owners;
   return (
     <div className="owners-container">
       <div className="owners-header">
         <h1>OWNERS </h1>
       </div>
-      <div className="cards-owner">
+      <div className="owner-cards">
         {owners.map((owner) => {
+          const ownername = sanitizeOwnername(owner.owner.ownername, " ");
           return (
-            <CardOwner
+            <Link
               key={owner.owner.ownerUid}
-              owner={owner.owner}
-              cabinets={owner.cabinets}
-            />
+              style={{ textDecoration: "none" }}
+              href={`owners/${ownername}`}
+            >
+              <CardOwner
+                key={owner.owner.ownerUid}
+                owner={owner.owner}
+                cabinets={owner.cabinets}
+              />
+            </Link>
           );
         })}
       </div>
