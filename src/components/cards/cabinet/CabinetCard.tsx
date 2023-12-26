808d5451e2063f5e9c5d81c816629a1a51c82cb6
Hulow <viq.hlw@gmail.com>
2023-12-26 12:43:44 +0100

    chore: change from flex to css grid

--- src/components/cards/cabinet/CabinetCard.tsx
+++ src/components/cards/cabinet/CabinetCard.tsx
@@ -1,3 +1,4 @@
+import { SecondaryLogoV2 } from "@/components/logo/Logo";
 import "./CabinetCard.css";
 import { CabinetCollectionOverview } from "@/types/cabinet";
 export function CabinetCard({ cabinet, drivers }: CabinetCollectionOverview) {
@@ -7,26 +8,16 @@ export function CabinetCard({ cabinet, drivers }: CabinetCollectionOverview) {
         <p>{cabinet.productName}</p>
         <p>{cabinet.enclosureType}</p>
       </div>
-      <div className="drivers-body">
-        {drivers.map((driver) => {
-          return <p key={driver.driverUid}>{driver.productName}</p>;
-        })}
+      <div className="drivers-content">
+        <div className="drivers-logo">
+          <SecondaryLogoV2 />
+        </div>
+        <div className="drivers-body">
+          {drivers.map((driver) => {
+            return <p key={driver.driverUid}>{driver.productName}</p>;
+          })}
+        </div>
       </div>
     </div>
   );
-}
-
-export interface CabinetOverview {
-  cabinetUid: string;
-  brandName: string;
-  productName: string;
-  enclosureType: string;
-  createdAt: Date;
-}
-
-export interface DriverOverview {
-  driverUid: string;
-  brandName: string;
-  productName: string;
-  driverType: string;
 }
