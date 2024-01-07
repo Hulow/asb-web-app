ed8580ede52635ffadb28afd1ecb0570d01617aa
Hulow <viq.hlw@gmail.com>
2024-01-07 19:31:30 +0100

    add chartjs Line chart

--- src/app/owners/[ownername]/measurement/[cabinetUid]/page.tsx
+++ src/app/owners/[ownername]/measurement/[cabinetUid]/page.tsx
@@ -1,9 +1,12 @@
+"use client";
 import "./page.css";
 import { httpClient } from "@/lib/http-client";
 import { config } from "@/config/config";
 import { Measurement } from "@/types/measurement";
 import { Cabinet } from "@/components/cabinet/Cabinet";
 import { Driver } from "@/components/driver/Driver";
+import { Chart } from "@/components/chart/Chart";
+import { useEffect, useRef, useState } from "react";
 
 interface MeasurementParam {
   params: {
@@ -11,11 +14,34 @@ interface MeasurementParam {
   };
 }
 
-export default async function MeasurementPage({ params }: MeasurementParam) {
+export default function MeasurementPage({ params }: MeasurementParam) {
   const endpoint =
-    config.endpoints.measurement.measurementPerCabinet + params.cabinetUid;
-  const measurement: Measurement = await httpClient("get", endpoint);
+    config.asbBaseUrl +
+    config.endpoints.measurement.measurementPerCabinet +
+    params.cabinetUid;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(endpoint, {
+      method: "get",
+      headers: { Authorization: config.asbKeyUrl },
+      cache: "no-store",
+    })
+      .then((data) => data.json())
+      .then((data) => setData(data));
+  }, []);
+
+  if (!data) {
+    return (
+      <div>
+        <h1>hello</h1>
+      </div>
+    );
+  }
+
+  const measurement: Measurement = data;
   const { cabinet, drivers, frequency, impedance } = measurement;
+
   return (
     <div className="measurement-container">
       <div className="measurement-ttle">
@@ -29,6 +55,12 @@ export default async function MeasurementPage({ params }: MeasurementParam) {
             return <Driver key={driver.uid} {...driver}></Driver>;
           })}
         </div>
+      </div>
+      <div className="frequency-container">
+        <div className="frequency-title">
+          <h2>Frequency Response</h2>
+        </div>
+        <div className="chart-container">{<Chart {...frequency} />}</div>
       </div>
     </div>
   );
--- src/lib/http-client.ts
+++ src/lib/http-client.ts
@@ -5,17 +5,23 @@ export interface FetchOptions {
   headers: {
     Authorization: string;
   };
+  cache: string;
 }
 
 export async function httpClient(method: string, endpoint: string) {
   const url = config.asbBaseUrl + endpoint;
 
-  const options: FetchOptions = {
+  const options = {
     method: method,
     headers: {
       Authorization: config.asbKeyUrl,
     },
+    cache: "no-cache",
   };
-  const response = await fetch(url, options);
+  const response = await fetch(url, {
+    method: options.method,
+    headers: { Authorization: options.headers.Authorization },
+    cache: "no-store",
+  });
   return await response.json();
 }
